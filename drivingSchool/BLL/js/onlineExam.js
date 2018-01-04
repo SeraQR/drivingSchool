c8c887c669bcbc75573b71752f4a5c713b03b101
JinmaQAQ <jinmaup@gmail.com>
2018-01-04 10:50:39 +0800

    增加在线考试功能

--- drivingSchool/BLL/js/onlineExam.js
+++ drivingSchool/BLL/js/onlineExam.js
@@ -1,4 +1,5 @@
-﻿const header = document.getElementById("header"),
+﻿/* eslint-disable */
+const header = document.getElementById("header"),
     steps = 7;
 
 function threedee(e) {
@@ -22,7 +23,148 @@ function threedee(e) {
     header.style.MozTransform = `translateZ(0) rotateX(${y * 1.5}deg) rotateY(${-x * 1.5}deg)`;
 }
 $(".content-header").on("mousemove", threedee);
+const tip = $("#tip");
+const question = $(".question");
+const optionA = $(".optionA");
+const optionB = $(".optionB");
+const optionC = $(".optionC");
+const optionD = $(".optionD");
 
+let answer = "";
+let nowTure = 0;
+
+let index = 0;
+let oldTure;
 $(() => {
-    setTimeout("$(\"#loading\").fadeOut()", 800);
+    Promise.all([getOldTure(), loadQuestion()]).then(() => {
+        setTimeout("$(\"#loading\").fadeOut()", 800);
+    });
 });
+
+function loadQuestion() {
+    question.text(problem[index].question);
+    optionA.text(problem[index].optionA);
+    optionB.text(problem[index].optionB);
+    optionC.text(problem[index].optionC);
+    optionD.text(problem[index].optionD);
+}
+
+function getOldTure() {
+    oldTure = localStorage.getItem("oldTure");
+    if (!oldTure) {
+        oldTure = 0;
+        localStorage.setItem("oldTure", oldTure);
+    }
+    $("#oldTureNum").text(oldTure);
+}
+
+$(".option_a").change(() => {
+    if (!problem[index].submitted) {
+        showTip("");
+        $("input[type='radio']").removeClass("checked");
+        $(".option_a").addClass("checked");
+        if ($(".option_a").prop("checked")) {
+            answer = "A";
+        }
+    }
+});
+$(".option_b").change(() => {
+    if (!problem[index].submitted) {
+        showTip("");
+        $("input[type='radio']").removeClass("checked");
+        $(".option_b").addClass("checked");
+        if ($(".option_b").prop("checked")) {
+            answer = "B";
+        }
+    }
+});
+$(".option_c").change(() => {
+    if (!problem[index].submitted) {
+        showTip("");
+        $("input[type='radio']").removeClass("checked");
+        $(".option_c").addClass("checked");
+        if ($(".option_c").prop("checked")) {
+            answer = "C";
+        }
+    }
+});
+$(".option_d").change(() => {
+    if (!problem[index].submitted) {
+        showTip("");
+        $("input[type='radio']").removeClass("checked");
+        $(".option_d").addClass("checked");
+        if ($(".option_d").prop("checked")) {
+            answer = "D";
+        }
+    }
+});
+
+function showTip(info) {
+    tip.text(info);
+    info ? tip.fadeIn(500) : tip.fadeOut("slow");
+}
+$("#sure").click(() => {
+    if (problem[index].submitted) {
+        showTip("您已提交过了");
+    } else if (answer === "") {
+        showTip("请选择一个选项！");
+    } else {
+        problem[index].submitted = true;
+        if (answer !== problem[index].answer) {
+            showTip(problem[index].tip);
+        } else {
+            problem[index].isTure = true;
+            showTip("恭喜您，答对了！");
+            nowTure++;
+            if (nowTure > oldTure) {
+                oldTure = nowTure;
+                $("#oldTureNum").text(oldTure);
+                localStorage.setItem("oldTure", oldTure);
+            }
+            $("#nowTureNum").text(nowTure);
+        }
+        $(".option" + problem[index].answer).css("color", "green");
+    }
+});
+$(".prev").click(() => {
+    showTip("");
+    answer="";
+    index--;
+    if (index < 0) {
+        index = 0;
+    } else {
+        $("input[type='radio']").removeClass("checked");
+        $(".option").attr("style", "");
+        if (problem[index].submitted) {
+            $(".option" + problem[index].answer).css("color", "green");
+            $(".option_" + problem[index].answer.toLowerCase()).addClass("checked");
+            if (problem[index].isTure) {
+                showTip("恭喜您，答对了！");
+            } else {
+                showTip(problem[index].tip);
+            }
+        }
+        loadQuestion();
+    }
+})
+$(".next").click(() => {
+    showTip("");
+    answer="";
+    index++;
+    if (index > problem.length - 1) {
+        index = problem.length - 1;
+    } else {
+        $("input[type='radio']").removeClass("checked");
+        $(".option").attr("style", "");
+        if (problem[index].submitted) {
+            $(".option" + problem[index].answer).css("color", "green");
+            $(".option_" + problem[index].answer.toLowerCase()).addClass("checked");
+            if (problem[index].isTure) {
+                showTip("恭喜您，答对了！");
+            } else {
+                showTip(problem[index].tip);
+            }
+        }
+        loadQuestion();
+    }
+})
