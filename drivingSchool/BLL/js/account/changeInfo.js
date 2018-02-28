a592796313e9255966bbfd56bae8561b437c54c2
JinmaQAQ <jinmaup@gmail.com>
2018-02-28 19:00:58 +0800

    更新readme

--- drivingSchool/BLL/js/account/changeInfo.js
+++ drivingSchool/BLL/js/account/changeInfo.js
@@ -1,62 +1,71 @@
 ﻿import "formStyle/changeInfo";
-import { Form as _ } from "js/form/form";
-import { Account } from "js/ajax/account";
 import {
-    address,
-    description,
-    tipAddress,
-    tipDescription,
-    act,
-    isCoach
+  Form as _
+} from "js/form/form";
+import {
+  Account
+} from "js/ajax/account";
+import {
+  address,
+  description,
+  tipAddress,
+  tipDescription,
+  act,
+  isCoach
 } from "js/common/variable";
 
 const user = {
-    act,
-    isCoach,
-    address: "",
-    description: ""
+  act,
+  isCoach,
+  address: "",
+  description: ""
 };
 
-
 $(() => {
-    _.Init("50px 100px 0px 100px");
+  _.Init("50px 100px 0px 100px");
 });
 
 address.blur(() => {
-    user.address = $.trim(address.val());
-    if (user.address !== "") {
-        tipAddress.hide("slow");
-    } else {
-        tipAddress.show("slow");
-    }
+  user.address = $.trim(address.val());
+  if (user.address !== "") { 
+    tipAddress.hide("slow");
+  } else {
+    tipAddress.show("slow");
+  }
 });
 description.blur(() => {
-    user.description = $.trim(description.val());
-    if (user.description !== "") {
-        tipDescription.hide("slow");
-    } else {
-        tipDescription.show("slow");
-    }
+  user.description = $.trim(description.val());
+  if (user.description !== "") {
+    tipDescription.hide("slow");
+  } else {
+    tipDescription.show("slow");
+  }
 });
 $(document).keydown(e => {
-    if (e.keyCode === 13) {
-        user.description = $.trim(description.val());
-        user.address = $.trim(address.val());
-        changeInfo();
-    }
+  if (e.keyCode === 13) {
+    user.description = $.trim(description.val());
+    user.address = $.trim(address.val());
+    changeInfo();
+  }
 });
 
 $("#changeInfo").click(changeInfo);
 
 function changeInfo() {
-    if (!_.allHaveContent(user)) {
-        _.showTip("不能有空值！");
+  if (!_.allHaveContent(user)) {
+    _.showTip("不能有空值！");
+  } else {
+    if (Account.changeInfo(isCoach ? "coach" : "student", user)) {
+      _.showTip("修改成功！");
+      setTimeout(
+        () =>
+        location.href = `../${
+            user.isCoach ? "coach/coach" : "student/student"
+          }.html`,
+        800
+      );
     } else {
-        if (Account.changeInfo(isCoach ? "coach" : "student", user)) {
-            _.showTip("修改成功！");
-            setTimeout(() => location.href = `../${user.isCoach ? "coach/coach" : "student/student"}.html`, 800);
-        } else {
-            _.showTip("因不可抗因素注册失败~");
-        }
+      _.showTip("因不可抗因素注册失败~");
     }
+  }
 }
